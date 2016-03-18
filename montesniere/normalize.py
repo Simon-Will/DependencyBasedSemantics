fd816a9a0fed5eebaf3fbe9d2cc9d8e2eb90256a
HubReb <HubReb@users.noreply.github.com>
2016-03-18 02:23:11 +0100

    Added files via upload
    almost finished, still incapable of changing more than one Obj Conj Conj construction

--- montesniere/normalize.py
+++ montesniere/normalize.py
@@ -11,8 +11,8 @@ class Normalizer:
         sentence_parts  : list of lines of splitted sentence
         self.word_parts: list of attributes of one sentence_part
         self.verb : list of verbs in the sentence
-        self.sb : list of subject and subject modifiers in the sentence
-        
+        self.sb : list of subjects and subject modifiers in the sentence
+        self.obj list of object-Konj-object and object modifiers in the sentence 
         
     """
     
@@ -25,16 +25,21 @@ class Normalizer:
         Returns:
             the initialized Normalzer object
         Raises:
-            TODO
+            IOError
         
         """
+        self.obj = []
         self.verb = []
         self.sb = []
-        self.sentence = open(parsedSentence).read()
-        self.sentence_parts = self.sentence.split("\n")
-        self.word_parts = [part.split("\t") for part in self.sentence_parts]
-        self.word_parts.pop()
+        try:
+            self.sentence = open(parsedSentence).read()
+            self.sentence_parts = self.sentence.split("\n")
+            self.word_parts = [part.split("\t") for part in self.sentence_parts]
+            self.word_parts.pop()
         
+        except IOError:
+            raise IOError("file does not exist in specified folder")
+    
     def check_SB_Verb(self):
         """checks if number of verbs is equal to number of subjects"""
         for word in self.word_parts:
@@ -48,9 +53,36 @@ class Normalizer:
             
         return len(self.verb) <= len(self.sb)
     
+    def checkOB(self):
+        """checks if objects are directly connected"""
+        for word in self.word_parts:
+            
+            if word[7] == "OA" or word[7] == "DA":
+                obj = self.word_parts.index(word)
+                
+                if self.word_parts[obj+1][4] == "KON":
+                    
+                    self.obj.extend(self.checkfurtherObj(obj))
+    
+
+            
+    
+    def checkfurtherObj(self, obj):
+        n = obj+2
+        nextObj = [obj, obj+1]
+        while self.word_parts[n][7] == "NK":
+            modifierObj.append(n)
+            n += 1
+        
+        if self.word_parts[n][7] == self.word_parts[obj][7]:
+            nextObj.append(n)
+        
+        return nextObj
+            
+    
     
     def checkAttributes(self, index):
-        """adds subject modifiers to subject list"""
+        """adds modifiers to subject list"""
         sb = []
         oIndex = index
         while True:
@@ -66,31 +98,75 @@ class Normalizer:
 
     def getSentence(self):
         """returns normalized sentence"""
+        self.checkOB()
         if self.check_SB_Verb():
+        
+            if self.obj != []:
+                self.insertObject()
+            
             self.checkPron()
             return self.orderSentence()
         
         self.checkPron()
-        new_sentence_parts = self.sentence_parts[:self.verb[-1]]
-        for s in self.sb[-1]:
-            new_sentence_parts.append(self.sentence_parts[s])
-        new_sentence_parts.extend(self.sentence_parts[self.verb[-1]:])
-        self.sentence_parts = new_sentence_parts
+        
+        if len(self.sb) != 0:
+            self.insertSubject()
+       
+        self.checkOB()
+        
+        if self.obj != []:
+            self.insertObject()
+            
         return self.orderSentence()
 
     
     def checkPron(self):
         """ replaces pronomina in the sentence"""
-        if self.word_parts[self.sb[-1][-1]][4] == "PPER":
-            pron = self.sb[-1][-1]
-            sb = self.sb[-2]
-            sentence_new = self.sentence_parts[:pron]
-            for s in sb:
-                sentence_new.append(self.sentence_parts[s])
-            sentence_new.extend(self.sentence_parts[pron+1:])
-            self.sentence_parts = sentence_new
+        subjects = list(self.sb)
+        while subjects != []:
+          
+            if self.word_parts[subjects[-1][-1]][4] == "PPER":
+                pron = subjects[-1][-1]
+                sb = subjects[-2]
+                sentence_new = self.sentence_parts[:pron]
+                for s in sb:
+                    sentence_new.append(self.sentence_parts[s])
+                sentence_new.extend(self.sentence_parts[pron+1:])
+                self.sentence_parts = sentence_new
+          
+            subjects.pop()
+            
+            
+    def insertSubject(self):
+        """ inserts subject before verb"""
+        verbNumber = 1
+        while self.sb != []:
+          
+            if self.word_parts[self.verb[-verbNumber]][7] != "SB":
+                new_sentence_parts = self.sentence_parts[:self.verb[-1]]
+                for s in self.sb[-1]:
+                    new_sentence_parts.append(self.sentence_parts[s])
+                new_sentence_parts.extend(self.sentence_parts[self.verb[-1]:])
+                self.sentence_parts = new_sentence_parts
+            verbNumber +=1
+          
+            if verbNumber > len(self.verb):
+                break
+          
+            self.sb.pop()
             
 
+
+    def insertObject(self):
+        """insert first half of the sentence in front of second obj"""
+        # TODO: more than just one object
+        new_sentence_parts = self.sentence_parts[:self.obj[-1]]
+        # first object at self.ob[0]
+        new_sentence_parts.extend(self.sentence_parts[:self.obj[0]-1])
+        new_sentence_parts.extend(self.sentence_parts[self.obj[-1]:])
+        self.sentence_parts = new_sentence_parts
+        
+        
     def orderSentence(self):
         """ fixes line numbers"""
         import re
@@ -109,8 +185,12 @@ def testPronomina():
     gurken2 = Normalizer("../test/conll/tanzende_gurken2.conll")
     return gurken2.getSentence()
     
+def testObj():
+    hase = Normalizer("../test/conll/schenkender_hasen.conll")
+    return hase.getSentence()
     
         
 if __name__ == '__main__':
     print(test())
     print(testPronomina())
+    print(testObj())

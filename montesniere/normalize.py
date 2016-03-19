974f8d88625826216d5216e6817d74104d5af6b1
HubReb <HubReb@users.noreply.github.com>
2016-03-19 03:36:57 +0100

    dependency partly solved, ART stil problematic
    Art -> dependency

--- montesniere/normalize.py
+++ montesniere/normalize.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+
+# TO-DO: ART and MO
 class Normalizer:
     """Reads a parsed sentence out of a file and normalizes its structure
     A Normalizer object consists of a parsed sentence
@@ -12,8 +15,9 @@ class Normalizer:
         self.word_parts: list of attributes of one sentence_part
         self.verb : list of verbs in the sentence
         self.sb : list of subjects and subject modifiers in the sentence
-        self.obj list of object-Konj-object and object modifiers in the sentence 
-        
+        self.obj: list of object-Konj-object and object 
+            modifiers in the sentence 
+        self.pr : list of pron in sentence
     """
     
     def __init__(self, parsedSentence):
@@ -31,15 +35,17 @@ class Normalizer:
         self.obj = []
         self.verb = []
         self.sb = []
+        self.pr = []
+        
         try:
             self.sentence = open(parsedSentence).read()
-            self.sentence_parts = self.sentence.split("\n")
-            self.word_parts = [part.split("\t") for part in self.sentence_parts]
+            self.word_parts = [part.split("\t") for part in 
+                                self.sentence.split("\n")][:-1]
             self.word_parts.pop()
-        
+         
         except IOError:
-            raise IOError("file does not exist in specified folder")
-    
+            raise IOError("Could not find file")
+            
     def check_SB_Verb(self):
         """checks if number of verbs is equal to number of subjects"""
         for word in self.word_parts:
@@ -47,55 +53,208 @@ class Normalizer:
             if word[4] == "VVFIN":
                 self.verb.append(self.word_parts.index(word))
             
-            elif word[7] == "SB":
+            elif word[7] == "SB" and word[4] != "PPER":
                 # checks modifiers
-                self.sb.append(self.checkAttributes(self.word_parts.index(word)))
+                self.sb.append(self.checkAttributes(
+                                    self.word_parts.index(word))
+                                    )
+            
+            elif word[4] == "PPER":
+                self.pr.append(int(word[0]))
+                
             
         return len(self.verb) <= len(self.sb)
-    
-    def checkOB(self):
-        """checks if objects are directly connected"""
-        for word in self.word_parts:
-            
-            if word[7] == "OA" or word[7] == "DA":
-                obj = self.word_parts.index(word)
-                
-                if self.word_parts[obj+1][4] == "KON":
-                    
-                    self.obj.extend(self.checkfurtherObj(obj))
-    
-
-            
-    
-    def checkfurtherObj(self, obj):
-        n = obj+2
-        nextObj = [obj, obj+1]
-        while self.word_parts[n][7] == "NK":
-            modifierObj.append(n)
-            n += 1
         
-        if self.word_parts[n][7] == self.word_parts[obj][7]:
-            nextObj.append(n)
         
-        return nextObj
-            
-    
-    
     def checkAttributes(self, index):
         """adds modifiers to subject list"""
         sb = []
         oIndex = index
-        while True:
-            if self.word_parts[index-1][7] == "NK" or self.word_parts[index-1][7] == "PNC":
+        while index > 0:
+            if (self.word_parts[index-1][7] == "NK" or 
+                self.word_parts[index-1][7] == "PNC"):
                 sb.append(index-1)
                 index -= 1
             else:
                 break
         sb.append(oIndex)
         return sb
-            
-            
+        
 
+    def checkOB(self):
+        """checks if objects are directly connected"""
+        for word in self.word_parts:
+            
+            if word[7] == "OA" or word[7] == "DA" or word[7] == "OA2":
+                obj = self.word_parts.index(word)
+                
+                try:
+                    if self.word_parts[obj+1][4] == "KON":
+                        self.obj.append(self.checkfurtherObj(obj))
+                    
+                except IndexError:
+                    continue
+
+    def checkfurtherObj(self, obj):
+        """find coordinated object"""
+        n = obj+2
+        nextObj = []
+        nextObj.extend(self.checkAttributes(obj))
+        nextObj.extend([obj, obj+1])
+        while self.word_parts[n][7] == "NK":
+            nextObj.append(n)
+            n += 1
+        
+        if (self.word_parts[n][7] == self.word_parts[obj][7] or
+            self.word_parts[n][7] == "OA2"):
+            nextObj.append(n)
+        
+        return nextObj
+        
+    
+    
+    def checkPron(self):
+        """ replaces pronomina in the sentence"""
+        pkonj = []
+        for pron in self.pr:
+            pkonj.append(pron-1)
+            self.word_parts, words = self.word_parts[:pron-1], \
+                                        self.word_parts[pron:]
+            for i in range(pron,0,-1):
+                subj = [sub for sub in self.sb if i in sub]
+            self.word_parts.extend(self.addWords(subj))
+            self.word_parts.extend(words)
+            del subj[:]
+        self.word_parts = self.mergenewSentence(self.word_parts)
+        self.assignNodes(pkonj)
+            
+        
+        
+    def addWords(self, words):
+        """ add words to be inserted"""
+        appendedWords = []
+        for word in words:
+            for w in word:
+                appendedWords.append(self.word_parts[w])
+        return appendedWords
+    
+    def insertSubject(self):
+        """inserts subject before verb"""
+        vkonj = []
+        for verb in self.verb:
+            if self.word_parts[verb-1][7] != "SB":
+                vkonj.append(verb)
+                word_parts, words = self.word_parts[:verb], self.word_parts[verb:]
+                subj = [su for su in self.sb for i in range(verb,0,-1) 
+                            if i in su]
+                word_parts.extend(self.addWords(subj))
+                word_parts.extend(words)
+                self.word_parts = word_parts
+        self.word_parts = self.mergenewSentence(self.word_parts)
+        self.assignNodes(vkonj)
+        
+    def insertObject(self):
+        """insert first half of the sentence in front of second obj"""
+        konj = []
+        for obj in self.obj:
+            konj.extend([kon for kon in obj 
+                        if self.word_parts[kon][4] == "KON"])
+            for konjunction in konj:
+                word_parts, words = self.word_parts[:konjunction], \
+                                    self.word_parts[konjunction+1:]
+                word_parts.append(self.word_parts[konjunction])
+                word_parts.extend(self.word_parts[:obj[0]])
+                word_parts.extend(words)
+        self.word_parts = self.mergenewSentence(word_parts)
+        self.assignNodes(konj)
+            
+    def assignNodes(self, konj):
+        """assign correct dependencies"""
+        relations = {"SB" : [], "VVFIN" : [], "NN" : [], "OA" : [], 
+                    "OA2" : [], "DA" : [], "MO": []}
+        relations = self.assignRelations(self.word_parts, relations)
+        for word in self.word_parts:
+            if(self.checkNoun(word) or word[7] == "MO"):
+                word = self.assignVerb(word, relations, konj)
+            elif(word[7] == "NK"):
+                word = self.assignNK(word, relations, konj)
+            
+        
+    def checkNoun(self, word):
+        return word[4] == "NN"
+        
+        
+    def assignVerb(self, word, relations, konj):
+        """assign verb dependency"""
+        for n in relations["VVFIN"]:
+            for konjunction in konj:
+                if (int(word[0]) > konjunction and n > konjunction):
+                    self.changeWord(word, n)
+        return word
+            
+    
+    def assignNK(self, word, relations, konj):
+        """nomina dependency"""
+        for n in relations["SB"]:
+            for konjunction in konj:
+                if (int(word[0]) < konjunction and n < konjunction):
+                    if(int(word[0]) > n and word[3] == "DET"):
+                        continue
+                    self.changeWord(word, n)
+                    
+                elif (int(word[0]) > konjunction and n > konjunction):
+                    if(int(word[0]) > n and word[3] == "DET"):
+                        continue
+                    self.changeWord(word, n)
+                    
+        return word
+            
+    def assignRelations(self, words, relations):
+        """selects correct dependency"""
+        for key in relations.keys():
+            values = []
+            if key == "VVFIN" or key == "NN":
+                values.extend([int(word[0]) for word in words 
+                                if word[4] == key])
+            else:
+                values.extend([int(word[0]) for word in words 
+                                if word[7] == key])
+            relations[key] = values
+        return relations         
+    
+    def changeWord(self, word, n):
+        """cahnge dependency"""
+        word[6] = str(n)
+        return word
+    
+    def orderSentence(self):
+        """ fixes line numbers"""
+        sentence_p = []
+        for word in self.word_parts:
+            sentence_p.append(("\t").join(word))
+        sentence = ""
+        for line in sentence_p:
+            sentence += line + "\n"
+        return sentence
+    
+    def mergenewSentence(self, words):
+        """combines sentence"""
+        import re
+        sentence_part = []
+        sentence = ""
+        for word in words:
+            sentence_part.append(("\t").join(word))
+        
+        number_of_lines = len(sentence_part)
+        for i in range(1,number_of_lines+1):
+            line = re.sub("\S*\t",  str(i) + "\t", sentence_part[i-1], 1)
+            sentence += line +"\n" 
+        return [part.split("\t") for part in sentence.split("\n")][:-1]
+    
+        
+        
+        
+            
     def getSentence(self):
         """returns normalized sentence"""
         self.checkOB()
@@ -103,80 +262,32 @@ class Normalizer:
         
             if self.obj != []:
                 self.insertObject()
-            
+        
             self.checkPron()
             return self.orderSentence()
         
-        self.checkPron()
         
         if len(self.sb) != 0:
             self.insertSubject()
-       
-        self.checkOB()
+        
+        
+        self.checkPron()
+        
         
         if self.obj != []:
             self.insertObject()
+    
+    #    self.assignNodes()
             
         return self.orderSentence()
-
-    
-    def checkPron(self):
-        """ replaces pronomina in the sentence"""
-        subjects = list(self.sb)
-        while subjects != []:
-          
-            if self.word_parts[subjects[-1][-1]][4] == "PPER":
-                pron = subjects[-1][-1]
-                sb = subjects[-2]
-                sentence_new = self.sentence_parts[:pron]
-                for s in sb:
-                    sentence_new.append(self.sentence_parts[s])
-                sentence_new.extend(self.sentence_parts[pron+1:])
-                self.sentence_parts = sentence_new
-          
-            subjects.pop()
-            
-            
-    def insertSubject(self):
-        """ inserts subject before verb"""
-        verbNumber = 1
-        while self.sb != []:
-          
-            if self.word_parts[self.verb[-verbNumber]][7] != "SB":
-                new_sentence_parts = self.sentence_parts[:self.verb[-1]]
-                for s in self.sb[-1]:
-                    new_sentence_parts.append(self.sentence_parts[s])
-                new_sentence_parts.extend(self.sentence_parts[self.verb[-1]:])
-                self.sentence_parts = new_sentence_parts
-            verbNumber +=1
-          
-            if verbNumber > len(self.verb):
-                break
-          
-            self.sb.pop()
-            
-
-
-    def insertObject(self):
-        """insert first half of the sentence in front of second obj"""
-        # TODO: more than just one object
-        new_sentence_parts = self.sentence_parts[:self.obj[-1]]
-        # first object at self.ob[0]
-        new_sentence_parts.extend(self.sentence_parts[:self.obj[0]-1])
-        new_sentence_parts.extend(self.sentence_parts[self.obj[-1]:])
-        self.sentence_parts = new_sentence_parts
-        
-        
-    def orderSentence(self):
-        """ fixes line numbers"""
-        import re
-        number_of_lines = len(self.sentence_parts)
-        sentence = ""
-        for i in range(1,number_of_lines):
-            line = re.sub("\S*\t", str(i) + "\t", self.sentence_parts[i-1], 1)
-            sentence += line +"\n"
-        return sentence
-        
+     
+     
+     
+     
+     
+     
+     
+     
 def test():
     gurken = Normalizer("../test/conll/tanzende_gurken.conll")
     return gurken.getSentence() 
@@ -188,9 +299,9 @@ def testPronomina():
 def testObj():
     hase = Normalizer("../test/conll/schenkender_hasen.conll")
     return hase.getSentence()
-    
-        
+   
 if __name__ == '__main__':
     print(test())
     print(testPronomina())
     print(testObj())
+

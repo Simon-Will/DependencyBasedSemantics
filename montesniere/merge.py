aa208def9401452d36cda83b424cb3bd73e8d739
Simon-Will <will@cl.uni-heidelberg.de>
2016-03-20 11:17:47 +0100

    Make merge.py more flexible. Doesn't work yet

--- montesniere/merge.py
+++ montesniere/merge.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8
 
+import copy
+import itertools
+
 from nltk.grammar import DependencyGrammar
 import nltk.parse as nlp
 import nltk.sem.logic as nll
@@ -18,7 +21,7 @@ class SemMerger:
         dependencies: a list of nodes being currenlty processed
     '''
         
-    def __init__(self, dependencyGraph):
+    def __init__(self, depGraph):
         '''Initializes SemMerger with given values. 
         dependencyGraph has to be an preprocessed DependencyGraph object
         with each node having a logical expression (semrep)
@@ -31,7 +34,8 @@ class SemMerger:
         Raises:
             TypeError: dg is not a DependencyGraph object
         '''
-        self.dg = dependencyGraph
+        self.dg = depGraph
+        self.root = self.getRoot()
         self.dependencies = []
         # check type
         if isinstance(self.dg, nlp.dependencygraph.DependencyGraph):
@@ -40,85 +44,94 @@ class SemMerger:
         else:
             raise TypeError('argument must be a dependencygraph')
             
-        
-    def getDependencies(self):
-        '''find bottom of depedencytree and store those nodes in 
-        self.dependencies
-        '''
-        for node in self.dg.nodes:
-            
-            if self.dg.nodes[node]['deps'] == {}:
-                self.dependencies.append(node)
-                
-        # solve object-verb dependencies first
-        self.dependencies = list(reversed(self.dependencies))
-        
-    def getSemRepresentation(self):
-        ''' return logical expression of the entire sentence'''
-        # ensure that entire sentence is resolved
-        while len(self.dependencies) != 0:
-       
-            for node in self.dependencies:
-                self.sortSemantics(node)
-       
-        root = ourGetRoot(self.dg)
-        return root['semrep']
-            
-            
-    def sortSemantics(self, node):
-        '''gives each node the correct logical expression'''
-        self.dependencies.remove(node)
-        head = self.dg.nodes[node]['head']
-        
-        # stops the loop once the top of the tree has been reached
-        if self.dg.nodes[node]['address'] == 0:
-            return
-        
-        # fuses two representations of one entity (NE: first and last name)
-        elif (self.dg.nodes[node]['rel'] == 'PNC' 
-            and self.dg.nodes[node]['ctag'] == 'NOUN'):
-            return self.doubleNamedEntity(self.dg.nodes[head], 
-                                            self.dg.nodes[node])
-                                            
-    
-        
-        # subject-verb has to be last
-        elif(
-            self.dg.nodes[node]['rel'] == 'SB'
-            and len(self.dependencies) > 0
-            ):
-                self.dependencies.append(self.dg.nodes[node]['address'])
-                return
-        
-        # merge logical expressions
-        self.dg.nodes[head]['semrep'] = self.mergeSemantics(
-                                            self.dg.nodes[head],
-                                            self.dg.nodes[node]
-                                            )
-                                            
-        # if modified, continue branch
-        if (self.dg.nodes[node]['rel'] == 'NK'):
-            self.dependencies.insert(0, self.dg.nodes[head]['address'])
-                                            
-        elif self.dg.nodes[head]['address'] != None:
-            self.dependencies.append(self.dg.nodes[head]['address'])
-        
-        # starts (not quite) a loop
-        self.getSemRepresentation()
-    
-    def mergeSemantics(self, semhead, semdep):
-        '''merges logical expressions'''
-        # store final logical expression in Top-Node
-        if semhead['address'] == 0:
-            return semdep['semrep']
-            
-        try:
-            return self.applyCorrectly(semhead['semrep'], semdep['semrep'])
-        
-        except KeyError:
-            #in case of no logical representation
-            return semhead['semrep']
 
+    def mergeWithChildren(self, node, force=False):
+        """Merge the semrep of a node with the ones of its children.
+
+        Args:
+            node: A node of a dependencyGraph object.
+            force: A bool denoting if a merge should be forced, even if node
+                was merged already.
+        """
+        if self.isMerged(node) and not force:
+            # The node was already merged and a new merge will not be forced.
+            sys.stderr.write('The node was merged already')
+            return None
+
+        # Collect the children from self.dg.
+        children = self.getChildren(node)
+
+        print(node)
+        if len(children) == 0:
+            # If a node has no children, there is nothing to merge.
+            print(node)
+            try:
+                node['mergedsemrep'] = node['semrep']
+            except KeyError:
+                pass
+
+        else:
+            # Merge the children with their children first.
+            for c in children:
+                if not self.isMerged(c):
+                    self.mergeWithChildren(c, force)
+
+            # Now actually merge the node with its children.
+            mergeDict = {node['address']: node['semrep']}
+            mergeDict.update(
+                    {c['address']: c['mergedsemrep'] for c in children
+                        if 'mergedsemrep' in c})
+            node['mergedsemrep'] = self._merge(mergeDict)
+
+        # Mark node as merged and return.
+        node['merged'] = True
+
+    def _merge(self, mergeDict):
+        """Merge the lambda expressions in mergeDict.
+        
+        Args:
+            mergeDict: A dictionary mapping any keys to
+                nltk.sem.logic.LambdaExpression objects.
+        
+        Returns:
+            The Expression resulting from the merges.
+
+        Raises:
+            NoMergePossibleException if the types of the expressions
+                do not allow a merge.
+        """
+        # Don't change values of original dict. (Emulation of pass by value)
+        mergeDict = copy.deepcopy(mergeDict)
+
+        if len(mergeDict) == 1:
+            return list(mergeDict.values())[0]
+        else:
+            applicationTries = _generateApplicationTries(mergeDict.keys())
+            for t in applicationTries:
+                expr0, expr1 = mergeDict[t[0]], mergeDict[t[1]]
+                if isApplicableTo(expr0, expr1):
+                    firstMerge = expr0.applyto(expr1)
+                    newMergeDict = {
+                            k:v for k,v in mergeDict.items()
+                            if k != t[0] and k != t[1]
+                            }
+                    newMergeDict[t] = firstMerge
+                    try:
+                        merged = self._merge(newMergeDict)
+                    except NoMergePossibleException:
+                        continue
+                    return merged
+            else:
+                raise NoMergePossibleException
+
+    def isMerged(self, node):
+        """Check if a node's semrep has been merged with its children."""
+        try:
+            merged = True if node['merged'] is True else False
+        except KeyError:
+            # If the node has no key called 'merged', treat is as False.
+            merged = False
+        return merged
 
     def doubleNamedEntity(self, head_node, node):
         tlp = nll.LogicParser(type_check=True)
@@ -148,85 +161,107 @@ class SemMerger:
         exprSig = {key.format(head_node): val for key, val in sig.items()}
         head_node['semrep'] = tlp.parse(new_expr, signature=exprSig)
     
-    
-    def isApplicableTo(self, expr1, expr2):
-        '''Test whether expr1 can be applied to expr2.
-    
-        Args:
-            expr1: An nltk.sem.logic.Expression object with resolved type.
-            expr2: An nltk.sem.logic.Expression object with resolved type.
-
-        Returns:
-            True if expr1 can be applied to expr2; False otherwise.
-        '''
-        if expr1.type.first == expr2.type:
-            return True
-        
-        elif expr1.type.first.matches(expr2.type):
-            print("Warning: Type is not definite, application may be incorrect!")
-            return True
-        
-        return False
-
-
-    def applyCorrectly(self, expr1, expr2):
-        '''Try to correctly apply one expression to the other.
-
-        Args:
-            expr1: An nltk.sem.logic.Expression object with resolved type.
-            expr2: An nltk.sem.logic.Expression object with resolved type.
-
-        Returns:
-            An nltk.sem.logic.Expression object resulting from the
-                application of one expression to the other.
-    
-        Raises:
-            nltk.sem.logic.TypeResolutionException if none of the
-                expressions can be applied to the other.
-        ''' 
-        if self.isApplicableTo(expr1, expr2):
-            return expr1.applyto(expr2).simplify()
-        elif self.isApplicableTo(expr2, expr1):
-            return expr2.applyto(expr1).simplify()
-        else:
-            # either deps are wrong or types are not accurate
-            errMsg = 'The types {0} and {1} are incompatible.'
-            raise nll.TypeException(errMsg.format(expr1, expr2))
-    
     def getSemantics(self):
         ''' returns logical expression of the entire sentence '''
-        self.getDependencies()
-        return self.getSemRepresentation()
+        #self.getDependencies()
+        #return self.getSemRepresentation()
+        self.mergeWithChildren(self.root)
+        return self.root['mergedsemrep']
 
-def ourGetRoot(dependencyGraph):
-    '''Get the root node of a dependencyGraph.
-    
+    def getChildren(self, node):
+        """Get the child nodes of a node.
+
+        The addresses of the children are found under the 'deps' key in the
+        dict of the dependencyGraph self.dg.
+
+        Args:
+            node: A dict corresponding to a node in self.dg.
+
+        Returns:
+            A list of dicts corresponding to the child nodes.
+        """
+        deps = node['deps']
+        children = []
+        for child_list in deps.values():
+            children.extend([self.dg.nodes[c] for c in child_list])
+        return children
+
+    def getRoot(self):
+        '''Get the root node of the dependencyGraph.
+        
+        Returns:
+            The root node of the dependencyGraph self.dg.
+
+        Raises:
+            ValueError if self.dg has less than or more than one root node.
+        '''
+        roots = self.getChildren(self.dg.nodes[0])
+
+        if len(roots) == 1:
+            return roots[0]
+        elif len(roots) > 1:
+            errMsg = 'The dependencyGraph has more than one root.'
+            raise ValueError(errMsg)
+        else:
+            errMsg = 'The dependencyGraph has no root.'
+            raise ValueError(errMsg)
+
+def _generateApplicationTries(iterable):
+    for i in itertools.combinations(iterable, 2):
+        yield i
+        yield tuple(reversed(i))
+
+class NoMergePossibleException(Exception):
+    pass
+
+def isApplicableTo(expr1, expr2, strict=False):
+    '''Test whether expr1 can be applied to expr2.
+
     Args:
-        dependencyGraph: A nltk.parse.DependencyGraph object.
+        expr1: An nltk.sem.logic.Expression object with resolved type.
+        expr2: An nltk.sem.logic.Expression object with resolved type.
+
     Returns:
-        The root node of the dependencyGraph.
-    Raises:
-        ValueError if the dependencyGraph has less than or more than
-            root node.
+        True if expr1 can be applied to expr2; False otherwise.
     '''
-    root_dict = dependencyGraph.nodes[0]['deps']
-    roots = []
-    for root_list in root_dict.values():
-        roots.extend([r for r in root_list])
+    if expr1.type.first == expr2.type:
+        return True
+    
+    elif (not strict) and expr1.type.first.matches(expr2.type):
+        print("Warning: Type is not definite, application may be incorrect!")
+        return True
+    
+    return False
 
-    if len(roots) == 1:
-        return dependencyGraph.nodes[roots[0]]
-    elif len(roots) > 1:
-        errMsg = 'The dependencyGraph has more than one root.'
-        raise ValueError(errMsg)
+
+def applyCorrectly(expr1, expr2):
+    '''Try to correctly apply one expression to the other.
+
+    Args:
+        expr1: An nltk.sem.logic.Expression object with resolved type.
+        expr2: An nltk.sem.logic.Expression object with resolved type.
+
+    Returns:
+        An nltk.sem.logic.Expression object resulting from the
+            application of one expression to the other.
+
+    Raises:
+        nltk.sem.logic.TypeResolutionException if none of the
+            expressions can be applied to the other.
+    ''' 
+    if isApplicableTo(expr1, expr2):
+        return expr1.applyto(expr2).simplify()
+    elif isApplicableTo(expr2, expr1):
+        return expr2.applyto(expr1).simplify()
     else:
-        errMsg = 'The dependencyGraph has no root.'
-        raise ValueError(errMsg)
-
+        # either deps are wrong or types are not accurate
+        errMsg = 'The types {0} and {1} are incompatible.'
+        raise nll.TypeException(errMsg.format(expr1, expr2))
+    
 def testUsualCase():
     import nltk.sem.logic as nll
-    tlk = nll.LogicParser()
-    read_expr = tlk.parse
+    tlp = nll.LogicParser()
+    read_expr = tlp.parse
     treebank_data = open('../test/conll/beissende_taube.conll').read()
     dg = nlp.DependencyGraph(treebank_data)
     #this will soon be replaced by an automatic representation

93fb870e81c0155e46acc7560a8781a0ccee95f7
Simon-Will <will@cl.uni-heidelberg.de>
2016-03-24 13:56:40 +0100

    Add quotes to montesniere_get_Semantics.sh; use sh instead of bash

--- montesniere_get_Semantics.sh
+++ montesniere_get_Semantics.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -z "$1" ]; then
-	echo specify a conll-file in directory \'test\' as argument
+	echo "Specify a conll-file in directory 'test' as argument"
 	exit
 fi
 
-python3 initialize.py  $1
+python3 initialize.py  "$1"
 
 
